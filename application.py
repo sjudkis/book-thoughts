e5779387f098adaeef09d3666ff57f4c89b384e5
Sam Judkis <sjudkis@gmail.com>
2018-12-22 20:21:50 -0800

    able to view books and rating data, no reviews yet

--- application.py
+++ application.py
@@ -61,8 +61,11 @@ def index():
             message = "Invalid Password"
             return render_template("sign_in.html", message=message)
         
-        # correct password
+        # correct password, add username and id to session
         session['current_user'] = username
+        current_user_id = db.execute("SELECT id FROM users WHERE username = :username", {'username': username}).fetchone()[0]
+        session['current_user_id'] = current_user_id
+        
         return render_template("home.html", username=username)
 
 
@@ -70,9 +73,12 @@ def index():
 @app.route("/create_account", methods=["POST", "GET"])
 def create_account():
     
-    if request.method == "GET":
+    if request.method == "GET" and not session.get('current_user'):
         return render_template('create_account.html', message = '')
-    
+
+    elif request.method == "GET" and session.get('current_user'):
+        return redirect(url_for('index'))
+
     # for POST requests:
     #get form data
     create_username = request.form.get("username")
@@ -88,26 +94,34 @@ def create_account():
     
     # username available
     password_hash = generate_password_hash(create_password)
-    db.execute("INSERT INTO users (username, password_hash) VALUES (:username, :password_hash)", {"username":create_username, "password_hash":password_hash})
+    # inserts user into database, returns id of new user
+    new_user_id = db.execute("INSERT INTO users (username, password_hash) VALUES (:username, :password_hash) RETURNING id", 
+        {
+            "username":create_username, 
+            "password_hash":password_hash
+        }).fetchone()['id']
     db.commit()
     
+
     # add user to current session
     session['current_user'] = create_username
-
-    return render_template('create_success.html')
+    session['current_user_id'] = new_user_id
+    return render_template('create_success.html', id=session['current_user_id'])
 
 
 @app.route("/log_out", methods=["GET"])
 def log_out(): 
     session['current_user'] = None
+    session['current_user_id'] = None
     return render_template('logged_out.html')
 
 @app.route("/search", methods=["GET"])
 def search():
     
-    # if request.method == "GET":
-    #     return redirect(url_for('index'))
-    
+    # make sure user is logged in
+    if not session.get('current_user'):
+        return redirect(url_for('index'))
+
     search_text = request.args.get('search_text')
     search_type = request.args.get('search_type')
 
@@ -126,3 +140,45 @@ def search():
         return render_template('search_error.html', error_message="No books found")
     else:
         return render_template('search.html', all_books=all_books, search_type=search_type.upper(), search_text=search_text)
+
+@app.route("/book/<string:book_id>")
+def display_book(book_id):
+    # make sure user is logged in
+    if not session.get('current_user'):
+        return redirect(url_for('index'))
+
+    # get book info based on id from url string
+    book = db.execute("SELECT * FROM books WHERE id = :book_id", {"book_id": book_id}).fetchone()
+    if not book:
+        return render_template('error.html', error_message='Sorry, we can\'t find that book!')
+    
+    # get goodreads rating data from API
+    goodreads_url = 'https://www.goodreads.com/book/review_counts.json'
+    res = requests.get(goodreads_url, 
+        headers={'Accept': 'application/json'},
+        params={'key': goodreads_key, 'isbns': book.isbn, 'format': 'json'}
+        )
+    # extract book data from json
+    book_data = res.json()['books'][0]
+    if res.status_code == 200:
+        rating_data = {
+            'avg': book_data['average_rating'],
+            'count': book_data['work_ratings_count'],
+            'found': True
+        }
+    else:
+        rating_data = {'found': False}
+
+
+    # get user reviews from database
+    reviews = db.execute("SELECT * FROM reviews WHERE book_id = :book_id",
+        {'book_id': book_id}).fetchall()
+    # if no reviews found
+    if not reviews:
+        review_data = {'found': False}
+
+    return render_template('display_book.html', 
+        book=book, 
+        rating_data=rating_data, 
+        review_data=review_data
+        )
